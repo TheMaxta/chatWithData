1cd73609c9491191ccbf568950230c9948e7b20d
Maximillian Mahlke <themaxta@gmail.com>
2024-02-12 19:13:59 -0700

    setup vectordb with chroma

--- document_loading/load_doc.py
+++ document_loading/load_doc.py
@@ -1,5 +1,6 @@
 from langchain_community.document_loaders import PyPDFLoader
-
+from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain_community.vectorstores import Chroma
 
 PDF_PATH = '../documents/Rich-Dad-Poor-Dad.pdf'
 
@@ -9,3 +10,17 @@ loader = PyPDFLoader(PDF_PATH)
 pages = loader.load_and_split()
 
 print(pages[0])
+
+embedding_func = SentenceTransformerEmbeddings(
+    model_name="all-MiniLM-L6-v2"
+)
+
+# create vector store
+vectordb = Chroma.from_documents(
+    documents=pages,
+    embedding=embedding_func,
+    persist_directory=f"vector_db",
+    collection_name="rich_dad_poor_dad")
+
+# make vector store persistant
+vectordb.persist()
